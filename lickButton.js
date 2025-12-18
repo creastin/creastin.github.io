19b2b0646df9f50b461a2d1e7744f3e2ee8f7e55
creastin <41141593+creastin@users.noreply.github.com>
2025-12-18 10:48:42 +0800

    Add files via upload

---
+++ lickButton.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#lickButton").click(function(){
+        $("#needhide").toggle(400); // 使用jQuery的toggle方法自动切换显示/隐藏
+		$("#needhide1").toggle(400); // 使用jQuery的toggle方法自动切换显示/隐藏
+    });
+});
